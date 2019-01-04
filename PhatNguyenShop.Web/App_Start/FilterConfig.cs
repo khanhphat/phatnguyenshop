399dcda8b5bc0b7630c67c13a737bb436937770e
khanhphat <nguyenhuukhanhphat@gmail.com>
2019-01-04 21:41:14 +0700

    Project PhatNguyenShop

---
+++ PhatNguyenShop.Web/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace PhatNguyenShop.Web
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
---
+++ PhatNguyenShop.Web/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="PhatNguyenShop.Web.MvcApplication" Language="C#" %>
